9759c01b9dad0996f1d16d5121f14914f20b1765
move-geun <movegeun11@gmail.com>
2021-07-06 20:46:36 +0900

    practice 1

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const age = parseInt(prompt("How old are you?"));
+const logInBox =  document.querySelector(".login-box")
+const logInBoxInput = document.querySelector(".login-box input")
+const greeting = document.querySelector(".greeting")
 
-if(isNaN(age) || age<0){
-    console.log("Please write positive number ")
-} else if(age<=18){
-    console.log("You are so young")
-} else if(age>18 && age<=50){
-    console.log("You can drink")
-} else if(age>50 && age<=80){
-    console.log("You need a exercise")
-} else if(age===100){
-    console.log("You are wise")
-} else if(age>80){
-    console.log("You can do it whatever you want")
+
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = logInBoxInput.value;
+    localStorage.setItem("username","username");
+    logInBox.classList.add("hidden");
+   paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = 'Hello ${username}';
+    greeting.classList.remove("hidden");
+
+}
+
+const savedUserName = localStorage.getItem("username");
+
+if (savedUserName === null) {
+    logInBox.classList.remove("hidden");
+    logInBox.addEventListener("submit", onLoginSubmit);
+} else{
+   paintGreetings(savedUserName);
+
 }
