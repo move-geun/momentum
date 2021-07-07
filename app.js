f34f3f8c3c21ddb7dae0aa2bae05a65ad371886a
move-geun <movegeun11@gmail.com>
2021-07-07 22:23:06 +0900

    plus clock

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ const greeting = document.querySelector(".greeting")
 
 function onLoginSubmit(event){
     event.preventDefault();
-    const username = logInBoxInput.value;
-    localStorage.setItem("username","username");
     logInBox.classList.add("hidden");
-   paintGreetings(username);
+    const username = logInBoxInput.value;
+    localStorage.setItem("username",username);
+    paintGreetings(username);
 }
 
 function paintGreetings(username){
-    greeting.innerText = 'Hello ${username}';
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove("hidden");
 
 }
 
 const savedUserName = localStorage.getItem("username");
-
 if (savedUserName === null) {
     logInBox.classList.remove("hidden");
     logInBox.addEventListener("submit", onLoginSubmit);
 } else{
    paintGreetings(savedUserName);
-
 }
+
+console.log(savedUserName);
---
+++ clock.js
@@ -0,0 +1,14 @@
+const clcokTitle = document.querySelector(".clock")
+
+
+function clockTimer(){
+    const today = new Date();
+    const hours = String(today.getHours()).padStart(2,"0");
+    const minutes = String(today.getMinutes()).padStart(2,"0");
+    const seconds = String(today.getSeconds()).padStart(2,"0");
+    clcokTitle.innerText = `${hours}:${minutes}:${seconds}`
+    console.log(hours);
+}
+
+clockTimer();
+setInterval(clockTimer, 1000);
